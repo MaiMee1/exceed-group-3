bf0fc12141cad96230b93a0f2cd8bf58d4780c1a
magmagcup <47655424+magmagcup@users.noreply.github.com>
2019-07-13 02:26:06 +0700

    fix answer

--- scripts/training.js
+++ scripts/training.js
@@ -1,19 +1,15 @@
 const q1 = () =>{
-    alert('โรคซึมเศร้าเป็นโรคที่เกี่ยวกับความผิดปกติของสารเคมีในสมอง จึงสามารถสรุปได้ว่าโรคซึมเศร้าเป็นโรคที่ต้องมีการดูแลอย่างใกล้ชิด');
+    document.getElementById('answer').innerText = 'โรคซึมเศร้าเป็นโรคที่เกี่ยวกับความผิดปกติของสารเคมีในสมอง จึงสามารถสรุปได้ว่าโรคซึมเศร้าเป็นโรคที่ต้องมีการดูแลอย่างใกล้ชิด'
+    document.getElementById('answer').style.display = 'block'
 }
 
 const rightans = () =>{
-    alert('คุณตอบถูกยินดีด้วย!');
+    document.getElementById('answer').innerText ='คุณตอบถูกยินดีด้วย!'
+    document.getElementById('answer').style.display = 'block'
 }
 
 const q2 = () =>{
-    alert('ช็อตไฟฟ้าในสมอง เป็นการรักษาผู้ป่วยซึมเศร้า');
+    document.getElementById('answer').innerText ='ช็อตไฟฟ้าในสมอง,การกินยาปรับระดับสารเคมีในสมอง,การฝังเข็ม เป็นการรักษาผู้ป่วยซึมเศร้า'
+    document.getElementById('answer').style.display = 'block'
 }
 
-const q3 = () =>{
-    alert('การกินยาปรับระดับสารเคมีในสมอง เป็นการรักษาผู้ป่วยซึมเศร้า')
-}
-
-const q4 = () =>{
-    alert('กการฝังเข็ม เป็นการรักษาผู้ป่วยซึมเศร้า')
-}
