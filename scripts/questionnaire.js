95eb9c407b260c9fcb732622eac0b9ac4ef349b3
Anongnat.rat <anongnat.rat@ku.th>
2019-07-12 15:29:40 +0700

    nav +footer

--- scripts/questionnaire.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function(){
-    $("input[type=radio]").click(function(){
-        var index = $(this).index("input[type=radio]");
-        console.log(index, window.location.href);
-    });
-});
-
